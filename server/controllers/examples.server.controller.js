518f23d9108f5460728329704be81d65f40c9c3c
Joe Brody <joseph.brody@ufl.edu>
2021-02-28 02:03:49 -0500

    Sign up backend

---
+++ server/controllers/examples.server.controller.js
@@ -0,0 +1,5 @@
+const Example = require('../models/User.js')
+
+exports.hello = function(req, res) {
+    res.send('world')
+};
---
+++ server/routes/signup.js
@@ -0,0 +1,66 @@
+// This file handles all of the routing for user signup functionality.
+
+const User = require('../models/User');
+
+module.exports = (app) => {
+
+  app.post('/api/account/signup', (req, res, next) => {
+    
+    // Get the email and password from the request body
+    const { body } = req;
+    const { password } = body;
+    let {email} = body;
+    
+    if (!email) {
+      return res.send({
+        success: false,
+        message: 'Error: Email cannot be blank.'
+      });
+    }
+    if (!password) {
+      return res.send({
+        success: false,
+        message: 'Error: Password cannot be blank.'
+      });
+    }    
+    
+    email = email.toLowerCase();
+    email = email.trim();    
+    
+    // Verify that there is not already a user with the email address
+    User.find({
+      email: email
+    }, (err, previousUsers) => {
+      if (err) {
+        return res.send({
+          success: false,
+          message: 'Error: Server error'
+        });
+      } else if (previousUsers.length > 0) {
+        return res.send({
+          success: false,
+          message: 'Error: Account already exist.'
+        });
+      }      
+      
+      // Save the new user
+      const newUser = new User();      
+      newUser.email = email;
+      newUser.password = newUser.generateHash(password);
+
+      newUser.save((err, user) => {
+        if (err) {
+          return res.send({
+            success: false,
+            message: 'Error: Server error'
+          });
+        }
+        return res.send({
+          success: true,
+          message: 'Signed up'
+        });
+      });
+    });  
+
+    });
+};
