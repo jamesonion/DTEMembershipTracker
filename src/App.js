972fef76f856580a2b11a8305ed23508ad0622b8
sunnyyfs <sunny.yf987@gmail.com>
2021-03-16 13:29:37 -0400

    tried to add background img

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react'
 import './Assets/css/default.min.css'
+import './Assets/components/Navbar.js'
+import bgimg from './Assets/img/background.png'
 
 class App extends Component {
     render() {
         return (
-            <div className = "App">
-                <p>hello</p>
+            <div styles={{backgroundImage: `url(${bgimg})`}}>
+                heelo
             </div>
+            
 
         );
     }
