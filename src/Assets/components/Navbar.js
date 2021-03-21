f2c2228ed2bc00b30c3ac5deedd2c48d328f0ded
sunnyyfs <sunny.yf987@gmail.com>
2021-03-21 02:31:18 -0400

    nav bar (only need to fix postion)

--- src/Assets/components/Navbar.js
+++ src/Assets/components/Navbar.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "../css/default.min.css";
 import { Link, BrowserRouter as Router } from "react-router-dom";
+import iglogo from "../img/iglogo.png";
+import fblogo from "../img/fblogo.png";
 
 class Navbar extends Component {
   render() {
@@ -9,16 +11,19 @@ class Navbar extends Component {
         <nav className="navigation">
           <ul>
             <li>
-              <a href="/Mainpage">Not a member? Visit the DTE website.</a>
+              <div>
+                <a href="/Mainpage">Not a member? Create an Account now!</a>
+              </ div>
             </li>
-
             <li>
-              <a href="https://www.instagram.com/ufdreamteam/?hl=en">
-                Instagram
+              <a href="https://www.facebook.com/UFDreamTeam/">
+                <img className="fblogo" src={fblogo} />
               </a>
             </li>
             <li>
-              <a href="https://www.facebook.com/UFDreamTeam/">Facebook</a>
+              <a href="https://www.instagram.com/ufdreamteam/?hl=en">
+                <img className="inslogo" src={iglogo} />
+              </a>
             </li>
           </ul>
         </nav>
