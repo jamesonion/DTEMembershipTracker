7e500b6e92b01b22ecc88c2f00d986023777504f
James Nguyen <jamesqnguyen1@ufl.edu>
2021-03-20 23:23:02 -0400

    default signin

--- dte-tracker/src/App.js
+++ dte-tracker/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import Header from './components/Login'
-
+import Header from './components/SignIn'
 import './App.css';
 
 function App() {
--- dte-tracker/src/components/SignIn.js
+++ dte-tracker/src/components/SignIn.js
@@ -1,15 +1,23 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 import { useState } from 'react'
+import loginBackground from '../images/uh-health-shands-childrens-hospital_0.png'
 
 const Header = () => {
     return (
         <header>
+            <div>
+                <img src={loginBackground} alt="Shands Background"
+                style ={{
+
+                }}
+                />
+            </div>
             <h1>User Login</h1>
             <div className = "form">
                 <div className = "form-group">
-                    <label htmlFor="username"> Username </label>
-                    <input type="text" id = "username" placeholder = "user@gmail.com" />
+                    <label htmlFor="username"> Email </label>
+                    <input type="text" id = "username" placeholder = "example@gmail.com" />
                 </div>
                 <div className = "form-group">
                     <label htmlFor="password"> Password </label>
