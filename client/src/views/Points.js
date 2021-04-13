c31a0d71f889d32a6383f29d305182a3383ce9c7
sunnyyfs <sunny.yf987@gmail.com>
2021-04-12 21:41:54 -0400

    css for poinrs

--- client/src/views/Points.js
+++ client/src/views/Points.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Usernavigationbar from "../components/Navbar/UserNavbar.js";
 import { ListGroup } from "react-bootstrap";
+import "../default.min.css";
 
 const Points = (props) => {
   return (
@@ -12,28 +13,21 @@ const Points = (props) => {
           textAlign: "center",
           padding: "1rem",
           paddingBottom: "2rem",
+          fontSize: "50pt",
+          textShadow: " 2px 2px 8px #000000",
         }}
       >
         Points
       </h1>
       {/* Create red box with white outline giving the user's name */}
       <div className="pointsForm">
-        <h2
-          style={{
-            color: "white",
-            padding: "3rem",
-            fontWeight: "bold",
-          }}
-        >
-          YOUR NAME
-        </h2>
-
+        <h2 className="navbarFont">YOUR NAME</h2>
         {/* Can use a similar approach to the forms by creating another div here and have the following left-align in a smaller inner div */}
-        <h3 className="pointsField"> Meeting Points: meetingPoints </h3>
-        <h3 className="pointsField"> Event Points: eventPoints </h3>
-        <h3 className="pointsField"> Total Points: totalPoints </h3>
-        <h3 className="pointsField"> Requirement Status : status </h3>
-
+        <h3 className="navbarFont"> Meeting Points: {props.meetingPoints} </h3>
+        <h3 className="navbarFont"> Event Points: {props.eventPoints} </h3>
+        <h1 className="navbarFont"> Total Points: {props.totalPoints} </h1>
+        <br /> <br />
+        <h1 className="navbarFont"> Requirement Status : {props.status}</h1>
         {/* Experimenting with using a list group from bootstrap but likely not work */}
         {/*
         <ListGroup>
