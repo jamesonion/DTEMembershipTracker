8e32c9ed73a1f1cbc8283e45fc27100abfc4c2ff
James Nguyen <jamesqnguyen1@ufl.edu>
2021-04-13 12:40:55 -0400

    Created Dummy Submit Page
    Changelog:
    Submit:
    - added iframe tag with same style as events page
    
    Events:
    - changed border styling, but this does not work

--- client/src/views/Events.js
+++ client/src/views/Events.js
@@ -28,7 +28,9 @@ const Events = () => {
           src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FNew_York&amp;src=am9zZXBoYnJvZHkxMUBnbWFpbC5jb20&amp;src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;src=Nm1rbXJrYjZpNG5oYzdqZmEwdGcxM210OW9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=ZHJlYW10ZWFtZW5nLm1lbWJlcnNoaXBAZ21haWwuY29t&amp;color=%23039BE5&amp;color=%2333B679&amp;color=%230B8043&amp;color=%23D50000&amp;color=%23A79B8E&amp;showTitle=0&amp;showNav=1&amp;showPrint=1"
           frameBorder="0"
           style={{
-            border: "solid 1px #777",
+            borderColor: "#777",
+            borderStyle: "solid",
+            borderRadius: "1px",
             width: "100vh",
             height: "75vh",
           }}
